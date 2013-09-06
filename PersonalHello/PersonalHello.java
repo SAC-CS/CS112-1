de2022f13b6de07c152593aa1b281c533c016bcd
lambynguyen <lambynguyen@gmail.com>
2013-09-05 23:27:46 -0700

    Create PersonalHello.java

---
+++ PersonalHello/PersonalHello.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class PersonalHello 
+{	
+  public static void main(String[] args) 
+    {
+	     String UserName;
+
+	     System.out.println("My name is Java. What is your name? ");
+		
+	     Scanner in = new Scanner(System.in);
+	     UserName = in.next();
+
+       System.out.println("Hello " + UserName + "! Have a good day!");
+
+    }
+
+}
